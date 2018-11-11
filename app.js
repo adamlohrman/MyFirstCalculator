1e1ceb2d0baf19ad86bd6acde5fa19e47e8b6f85
Adam Lohrman <adamlohrman@gmail.com>
2018-11-10 21:16:36 -0600

    Well, I've gotten the addition to work how I want, now I have to get the subtraction to do it.

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.display');
 const calculate = document.querySelector('.calculate');   
 const plusButton = document.querySelector('.buttonPlus');
+const minusButton = document.querySelector('.buttonMinus');
 const equalsButton = document.querySelector('.buttonEquals');
 const clearButton = document.querySelector('.buttonClear');
 clearButton.addEventListener('click', clearAll);
 calculator.addEventListener('click', displayMe);
-plusButton.addEventListener('click', addMe);   
+plusButton.addEventListener('click', addMe);
+minusButton.addEventListener('click', subtractMe)   
 equalsButton.addEventListener('click', equalizeMe);
-const allNum = [];
+let myArr = [];
 let firstNum = '';
-let secondNum = '';
+let lastNum = '';
+
+// EVENT LISTENER FUNCTIONS
+// =================================================================================================================================
    
 function displayMe(e) {
    display.textContent += parseInt(e.target.textContent);
@@ -19,26 +24,59 @@ function displayMe(e) {
 
 function addMe(e) {
    firstNum = parseInt(display.textContent);
+   myArr.push(firstNum);
    display.textContent = '';
+   console.log(myArr);
    
    e.preventDefault();
 }
 
-function getSum(total, sum) {
-   return total + sum;
+function subtractMe(e) {
+   firstNum = parseInt(display.textContent);
+   myArr.push(firstNum);
+   display.textContent = '';
+   console.log(myArr);
+
+   e.preventDefault();
+}
+
+// ARRAY FUNCTIONS AND MATH EQUALITY
+// ===================================================================================================================================
+
+function getSum(total, num) {
+   return total + num;
+}
+
+function getSubT(total, num) {
+   return total - num;
 }
 
 function equalizeMe(e) {
-   secondNum = parseInt(display.textContent);
-   display.textContent = firstNum + secondNum;
+   if(display.textContent === Number) {
+      lastNum = parseInt(display.textContent);
+      myArr.push(lastNum);
+   } else {
+
+      if(addMe) {
+      display.textContent = myArr.reduce(getSum);
+      myArr = [];
+
+   }  if(subtractMe) {
+      console.log('its working');
+      display.textContent = myArr[1] - myArr[2];
+      myArr = [];
+   } 
+}
+   
+
    e.preventDefault();
 }
 
 function clearAll() {
    display.textContent = '';
    firstNum = '';
-   secondNum = '';
-   allNum = [];
+   lastNum = '';
+   myArr = [];
 }
    
    
